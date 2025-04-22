43d12e7f4b63ad2ae0e7d5e6c1ad1f12f7761362
Huseynitsu <huseynquliyev395@gmail.com>
2025-04-22 17:05:45 +0400

    about

--- js/script.js
+++ js/script.js
@@ -118,3 +118,17 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(secondSection);
   }
 });
+
+const elements = document.querySelectorAll(".animate-on-scroll");
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+elements.forEach((el) => observer.observe(el));
